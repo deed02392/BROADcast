fa42efa2f73fb2ce57db84626cfcb5b0e3320675
garnetius <garnetius@users.noreply.github.com>
2017-07-30 19:43:45 +0300

    Update OpenVPN scripts

--- openvpn/tap/add.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-set TAP="C:\Program Files\TAP-Windows"
-%TAP%\bin\devcon.exe install %TAP%\driver\OemWin2k.inf tap0901
-
-pause
--- openvpn/tap/remove.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-set TAP="C:\Program Files\TAP-Windows"
-%TAP%\bin\devcon.exe remove tap0901
-
-pause
