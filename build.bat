6e25f8b734784bb8ebf0b48235088022ecef7f74
garnetius <garnetius@users.noreply.github.com>
2018-02-02 11:04:33 +0300

    Strip the built executable

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 
 :: Build the executable
-gcc.exe -O2 -mconsole -municode broadcast.c -o BROADcast.exe -lws2_32 -lIphlpapi
+gcc.exe -O2 -mconsole -municode -s broadcast.c -o BROADcast.exe -lws2_32 -lIphlpapi
